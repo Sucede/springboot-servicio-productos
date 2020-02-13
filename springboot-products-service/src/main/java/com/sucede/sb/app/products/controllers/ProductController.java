73d38745684d79539294a9ab1c9f49b94efca06d
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-13 18:36:18 +0100

    Dejamos preparado Ribbon tambien para trabajar con la implementacion de RestTemplate sin utilizar Feign

--- springboot-products-service/src/main/java/com/sucede/sb/app/products/controllers/ProductController.java
+++ springboot-products-service/src/main/java/com/sucede/sb/app/products/controllers/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,9 @@ public class ProductController {
 	
 	@Autowired
 	private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
 
 	@Autowired
 	private IProductService productService;
@@ -24,7 +28,8 @@ public class ProductController {
 	@GetMapping("/list")
 	public List<Product> list(){
 		return productService.findAll().stream().map(p ->{
-			p.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//p.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			p.setPort(port);
 			return p;
 		}).collect(Collectors.toList());
 	}
@@ -32,7 +37,8 @@ public class ProductController {
 	@GetMapping("/view/{id}")
 	public Product product(@PathVariable Long id) {
 		Product product = productService.findById(id);
-		product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		product.setPort(port);
 		return product;
 	}
 }
