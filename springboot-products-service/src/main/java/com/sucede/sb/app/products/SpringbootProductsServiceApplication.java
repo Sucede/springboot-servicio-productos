9350609167a2cf3847d74d42ee22217eae5ba0c4
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-14 13:07:17 +0100

    Establecemos el servicio como cliente Eureka y quitamos ribbon

--- springboot-products-service/src/main/java/com/sucede/sb/app/products/SpringbootProductsServiceApplication.java
+++ springboot-products-service/src/main/java/com/sucede/sb/app/products/SpringbootProductsServiceApplication.java
@@ -2,7 +2,9 @@ package com.sucede.sb.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootProductsServiceApplication {
 
